415202d6806c11c5c01f0b7600b53affdd601507
Vũ Minh Nguyên <PiscesKaze@gmail.com>
2024-10-16 17:40:34 +0000

    Refactor project configuration files

---
+++ server/api/auth/pisces.js
@@ -0,0 +1,34 @@
+import GoogleProvider from 'next-auth/providers/google';
+import WikimediaProvider from 'next-auth/providers/wikimedia';
+import FacebookProvider from 'next-auth/providers/facebook';
+import { NuxtAuthHandler } from '#auth'
+
+export default NuxtAuthHandler({
+    providers: [
+        // @ts-expect-error Use .default here for it to work during SSR.
+        GoogleProvider.default({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        WikimediaProvider.default({
+            clientId: process.env.MIRAHEZE_ID,
+            clientSecret: process.env.MIRAHEZE_SECRET,
+        }),
+        {
+            id: "miraheze",
+            name: "Miraheze",
+            type: "oauth",
+            token: "https://meta.miraheze.org/w/rest.php/oauth2/access_token",
+            userinfo: "https://meta.miraheze.org/w/rest.php/oauth2/resource/profile",
+            authorization: {
+              url: "https://meta.miraheze.org/w/rest.php/oauth2/authorize",
+              params: { scope: "" },
+            }
+        },
+        FacebookProvider.default({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
+        }),
+    ],
+    // Additional NextAuth configuration options
+});
